12d3cca32ab428ff780fb5c467f03e1d1b2d7361
Jun Hashimoto <jun.hashimoto@mixi.co.jp>
2017-12-22 11:07:15 +0900

    show window prompt when NG word config url is not defined

--- zendesk-incident-protector.user.js
+++ zendesk-incident-protector.user.js
@@ -9,4 +9,46 @@
 // ==/UserScript==
 
 (function() {
+  'use strict';
+
+  class NGWordManager {
+    constructor(localStorageKey) {
+      this.localStorageKey = localStorageKey;
+    }
+
+    isConfigURLEmpty() {
+      let configURL = localStorage.getItem(this.localStorageKey);
+      return configURL === null;
+    }
+    setConfigURL(arg) {
+      if (this.isValidConfigURL(arg)) {
+        localStorage.setItem(this.localStorageKey, arg);
+      }
+    }
+    isValidConfigURL(arg) {
+      let urlRegExp = new RegExp('^(https?|ftp)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)$');
+      return arg.match(urlRegExp) !== null;
+    }
+  }
+
+  // execute UserScript on browser, and export NGWordManager class on test
+  if (typeof window === 'object') {
+    const localStorageKey = 'zendeskIncidentProtectorConfigURL';
+    const isEmpty = (arg) => {
+      return arg === null || arg === undefined;
+    };
+
+    let ngWordManager = new NGWordManager(localStorageKey);
+    let runUserScript = () => {
+      if (ngWordManager.isConfigURLEmpty()) {
+        let configURL = window.prompt('[Zendesk 事故防止ツール]\nNGワードの設定が記載されたURLを指定してください', '');
+
+        ngWordManager.setConfigURL(configURL);
+      }
+    };
+
+    runUserScript();
+  } else {
+    module.exports = NGWordManager;
+  }
 })();
