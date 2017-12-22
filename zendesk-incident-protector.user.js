c9c71556bec7c600d305ab3b6f829b65099e24ed
Jun Hashimoto <jun.hashimoto@mixi.co.jp>
2017-12-22 11:06:25 +0900

    add initial zendesk-incident-protector.user.js

---
+++ zendesk-incident-protector.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name           Zendesk Incident Protector
+// @version        1.0.0
+// @description    Prevent replying to customer with specific NG keywords
+// @author         XFLAG Studio CRE Team
+// @include        https://*.zendesk.com/*
+// @exclude        https://*.zendesk.com/knowledge/*
+// @require        https://code.jquery.com/jquery-3.2.1.min.js
+// ==/UserScript==
+
+(function() {
+})();
