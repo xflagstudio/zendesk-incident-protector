14e5f23108355a214ede0ad50ff6e5782bfa553c
Jun Hashimoto <jun.hashimoto@mixi.co.jp>
2018-01-04 16:42:26 +0900

    exclude analytics.zendesk.com

--- zendesk-incident-protector.user.js
+++ zendesk-incident-protector.user.js
@@ -4,6 +4,7 @@
 // @description    Prevent replying to customer with specific NG keywords
 // @author         XFLAG Studio CRE Team
 // @include        https://*.zendesk.com/*
+// @exclude        https://analytics.zendesk.com/*
 // @exclude        https://*.zendesk.com/knowledge/*
 // @require        https://code.jquery.com/jquery-3.2.1.min.js
 // ==/UserScript==
